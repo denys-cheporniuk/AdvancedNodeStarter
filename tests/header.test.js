cf0b21da33fc8d13a2e1ad012f04a6cb8abdc9bd
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2023-10-26 15:01:15 +0300

    add test for login logic

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,24 +1,62 @@
-const puppeteer = require('puppeteer');
+const keys = require('../config/keys');
 
-describe('Browser launch and create a new page', () => {
+const puppeteer = require('puppeteer');
+const { Buffer } = require('safe-buffer');
+const Keygrip = require('keygrip');
+
+describe('Page header tests', () => {
   let browser;
   let page;
 
-
-  beforeAll(async () => {
+  beforeEach(async () => {
     browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
     await page.goto('localhost:3000');
   });
 
 
-  afterAll(async () => {
+  afterEach(async () => {
     await browser.close();
   });
 
-  test('Logo should have correct text', async () => {
+  test('The header logo should have correct text', async () => {
     const logoText = await page.$eval('a.brand-logo', el => el.innerHTML);
 
     expect(logoText).toEqual('Blogster');
-  })
+  });
+
+  test('Should redirect to google auth page after clicking on the login button', async () => {
+    await page.click('.right a');
+
+    const url = await page.url();
+
+    expect(url).toMatch(/accounts\.google\.com/);
+  });
+
+  test('Should show a logout button after successful login', async () => {
+    const userId = '6537d2da22161f3d092d8fa5';
+
+    const session = {
+      passport: {
+        user: userId,
+      }
+    };
+
+    const sessionString = Buffer.from(
+      JSON.stringify(session)
+    ).toString('base64');
+
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign(`session=${sessionString}`);
+
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+
+    await page.goto('localhost:3000');
+    await page.waitFor('a[href="/auth/logout"]');
+
+    const buttonText = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+    expect(buttonText).toEqual('Logout');
+  });
 })
